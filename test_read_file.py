a80488ed3b848c8314009c983c68b13e7e739ef2
Ludo <lraulin843@gmail.com>
2022-07-06 08:32:16 +0200

    removed test file

--- test_read_file.py
+++
@@ -1,9 +0,0 @@
-from jproperties import Properties
-
-configs = Properties()
-
-with open('config.properties', 'rb') as config_file:
-    configs.load(config_file)
-    
-print(configs.get("username").data)
-print(configs.get("password").data)
